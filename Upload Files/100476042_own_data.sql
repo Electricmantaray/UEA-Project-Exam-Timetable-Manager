63e1bb6b0097f206ec8a4667609d89e33000fec2
Electricmantaray <hlsj1066@gmail.com>
2025-05-02 02:40:36 +0100

    Commented and tested

--- Upload Files/100476042_own_data.sql
+++ Upload Files/100476042_own_data.sql
@@ -69,17 +69,34 @@ INSERT into cancel(eno, excode, sno, cdate, cuser) VALUES
 INSERT into exam(excode, extitle, exlocation, exdate, extime) VALUES
 -- Primary Key Violation (Duplicate excode)
 -- Output:
--- 
+    /* 
+    ERROR:  duplicate key value violates unique constraint "exam_pkey"
+    Key (excode)=(DB01) already exists. 
+    */
     ('DB01', 'Abnormal exam 1', 'Abnormal location', '2025-11-01', '09:00'),
 
 -- Duplicated Title Violation (extitle Not Unique)
 -- Output:
--- 
+    /* 
+    ERROR:  duplicate key value violates unique constraint "exam_extitle_key"
+    Key (extitle)=(Database system exam 2) already exists. 
+    */
     ('A001', 'Database system exam 2', 'Abnormal location', '2025-11-01', '9:00'),
 
 -- NULL Violation (excode, extitle, exlocation, exdate, extime)
 -- Output:
--- 
+    /* 
+    ERROR:  null value in column "excode" of relation "exam" violates not-null constraint
+    Failing row contains (null, Abnormal exam 2, Abnormal location, 2025-11-01, 09:00:00). 
+    ERROR:  null value in column "extitle" of relation "exam" violates not-null constraint
+    Failing row contains (A002, null, Abnormal location, 2025-11-01, 09:00:00). 
+    ERROR:  null value in column "exlocation" of relation "exam" violates not-null constraint
+    Failing row contains (A003, Abnormal exam 3, null, 2025-11-01, 09:00:00). 
+    ERROR:  null value in column "exdate" of relation "exam" violates not-null constraint
+    Failing row contains (A004, Abnormal exam 4, Abnormal location, null, 09:00:00). 
+    ERROR:  null value in column "extime" of relation "exam" violates not-null constraint
+    Failing row contains (A005, Abnormal exam 5, Abnormal location, 2025-11-01, null). 
+    */
     (NULL, 'Abnormal exam 2', 'Abnormal location', '2025-11-01', '09:00'),
     ('A002', NULL,  'Abnormal location', '2025-11-01', '09:00'),
     ('A003', 'Abnormal exam 3', NULL, '2025-11-01', '09:00'),
@@ -88,9 +105,12 @@ INSERT into exam(excode, extitle, exlocation, exdate, extime) VALUES
 
 -- Abnormally Long code/title and location (Greater than their limits)
 -- Output:
--- 
+    /*
+    ERROR:  value too long for type character(4) 
+    ERROR:  value too long for type character varying(200) 
+    */
     ('A9999', 'Abnormal exam 9999', 'Abnormal location',  '2025-11-01', '09:00'),
-    ('A0006', 'Lorem ipsum dolor sit amet consectetur adipiscing 
+    ('A006', 'Lorem ipsum dolor sit amet consectetur adipiscing 
     elit quisque faucibus ex sapien vitae pellentesque sem 
     placerat in id cursus mi pretium tellus duis convallis 
     tempus leo eu aenean sed diam urna tempor',
@@ -102,7 +122,12 @@ INSERT into exam(excode, extitle, exlocation, exdate, extime) VALUES
 
 -- Constraint Violations (exdate outside of November 2025, extime outside the 09:00-18:00)
 -- Output:
--- 
+    /*
+    ERROR:  new row for relation "exam" violates check constraint "exam_exdate_check"
+    Failing row contains (A007, Abnormal exam 7, Abnormal location, 2024-12-25, 09:00:00).
+    ERROR:  new row for relation "exam" violates check constraint "exam_extime_check"
+    Failing row contains (A008, Abnormal exam 8, Abnormal location, 2025-11-01, 21:00:00). 
+    */
     ('A007', 'Abnormal exam 7', 'Abnormal location', '2024-12-25', '09:00'),
     ('A008', 'Abnormal exam 8', 'Abnormal location', '2025-11-01', '21:00');
 
@@ -111,31 +136,48 @@ INSERT into exam(excode, extitle, exlocation, exdate, extime) VALUES
 INSERT into student(sno, sname, semail) VALUES
 -- Primary Key Violation (Duplicate sno)
 -- Output: 
--- 
+    /*
+    ERROR:  duplicate key value violates unique constraint "student_pkey"
+    Key (sno)=(1) already exists. 
+    */
     (1, 'Abnormal Student', 'Abnormal1@example.edu'),
 
 -- Duplicated Email Violation (semail Not Unique)
 -- Output: 
--- 
-    (2, 'Abnormal Student', 'lnguyen17@example.edu'),
+    /*
+    ERROR:  duplicate key value violates unique constraint "student_semail_key"
+    Key (semail)=(lnguyen17@example.edu) already exists. 
+    */
+    (9999, 'Abnormal Student', 'lnguyen17@example.edu'),
 
 -- Empty Violation (sno, sname, semail)
 -- Output:
--- 
+    /*
+    ERROR:  null value in column "sno" of relation "student" violates not-null constraint
+    Failing row contains (null, Void1, void1@example.edu). 
+    ERROR:  null value in column "sname" of relation "student" violates not-null constraint
+    Failing row contains (9998, null, Void2@example.edu). 
+    ERROR:  null value in column "semail" of relation "student" violates not-null constraint
+    Failing row contains (9997, Void3, null). 
+    */
     (NULL, 'Void1', 'void1@example.edu'),
-    (9999, NULL, 'Void2@example.edu'),
-    (9998, 'Void3', NULL),
+    (9998, NULL, 'Void2@example.edu'),
+    (9997, 'Void3', NULL),
 
 -- Invalid Entry Type (INTEGER -> CHAR, CHAR -> INTEGER)
 -- Output:
--- 
-    ('1', 'Abnormal Student', 'abnormal2@example.edu'),
-    (9997, 1234, 1234),
+    /*
+    Data type is automatically corrected and inserted
+    */
+    ('9996', 'Abnormal Student', 'abnormal2@example.edu'),
+    (9995, 1234, 1234),
 
 -- Abnormally Long names and emails (Greater than 200 characters)
 -- Output:
--- 
-    (9996, 'Lorem ipsum dolor sit amet consectetur adipiscing 
+    /*
+    ERROR:  value too long for type character varying(200) 
+    */
+    (9994, 'Lorem ipsum dolor sit amet consectetur adipiscing 
     elit quisque faucibus ex sapien vitae pellentesque sem 
     placerat in id cursus mi pretium tellus duis convallis 
     tempus leo eu aenean sed diam urna tempor', 
@@ -149,51 +191,86 @@ INSERT into student(sno, sname, semail) VALUES
 INSERT into entry(eno, excode, sno, egrade) VALUES
 -- Primary Key Violation (Duplicate eno)
 -- Output: 
--- 
+    /*
+    ERROR:  duplicate key value violates unique constraint "entry_pkey"
+    Key (eno)=(1) already exists.
+    */
     (1, 'DB02', 1, NULL),
 
 -- Duplicated Entrys Violations (Multiple entrys for the same student and exam)
 -- Output: 
--- 
+    /*
+    PLACEHOLDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    */
     (9999, 'DB01', 1, NULL),
     (9998, 'DB01', 1, NULL),
 
 -- Empty Violation (eno, excode, sno)
 -- Output:
--- 
+    /*
+    ERROR:  null value in column "eno" of relation "entry" violates not-null constraint
+    Failing row contains (null, DB01, 2, null). 
+    ERROR:  null value in column "excode" of relation "entry" violates not-null constraint
+    Failing row contains (9997, null, 3, null). 
+    ERROR:  null value in column "sno" of relation "entry" violates not-null constraint
+    Failing row contains (9996, DB03, null, null). 
+    */
     (NULL, 'DB01', 2, NULL),
     (9997, NULL, 3, NULL),
     (9996, 'DB03', NULL, NULL),
 
 -- Foreign Key Violation (Non-existent excode or sno)
 -- Output:
---
+    /*
+    ERROR:  insert or update on table "entry" violates foreign key constraint "fk_entry_exam"
+    Key (excode)=(NONE) is not present in table "exam".
+    ERROR:  insert or update on table "entry" violates foreign key constraint "fk_entry_student"
+    Key (sno)=(7421) is not present in table "student"
+    */
     (9995, 'NONE', 4, NULL),
-    (9994, 'AI02' 7421, NULL),
+    (9994, 'AI02', 7421, NULL),
 
 -- Same Day Exam Violation (Same student has multiple exams on the same day)
--- Output:
---
+-- Output: 
+    /*
+    PLACEHOLDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    */
     (9993, 'DB02', 6, NULL),
     (9992, 'WP01', 6, NULL),
 
 -- Constraint Violation (egrade outside of 0-100 bounds, egrade with 3 or more decimal places)
 -- Output:
--- 
+    /*
+    ERROR:  new row for relation "entry" violates check constraint "entry_egrade_check"
+    Failing row contains (9991, MA01, 4, 101.00). 
+    Rounds the decimal places to 2, 50.5555 -> 50.56
+    */
     (9991, 'MA01', 4, 101.00),
-    (9990, 'SP01', 7, 50.5555);
+    (9990, 'SD01', 7, 50.5555);
 
 
 -- CANCEL --
 INSERT into cancel(eno, excode, sno, cdate, cuser) VALUES
 -- Primary Key Violation (Duplicate)
 -- Output:
--- 
+    /*
+    ERROR:  duplicate key value violates unique constraint "cancel_pkey"
+    Key (eno)=(11) already exists. 
+    */
     (11, 'MA01', 10, '2025-01-01 00:00:00', 'admin'),
 
 -- Empty Violation (eno, excode, sno, cuser)
 -- Output:
--- 
+    /*
+    ERROR:  null value in column "eno" of relation "cancel" violates not-null constraint
+    Failing row contains (null, SD02, 5, 2025-01-01 00:00:00, admin).
+    ERROR:  null value in column "excode" of relation "cancel" violates not-null constraint
+    Failing row contains (9999, null, 3, 2025-01-01 00:00:00, admin). 
+    ERROR:  null value in column "sno" of relation "cancel" violates not-null constraint
+    Failing row contains (9998, DB03, null, 2025-01-01 00:00:00, admin).
+    ERROR:  null value in column "cuser" of relation "cancel" violates not-null constraint
+    Failing row contains (9997, AI02, 3, 2025-01-01 00:00:00, null). 
+    */
     (NULL, 'SD02', 5, '2025-01-01 00:00:00', 'admin'),
     (9999, NULL, 3, '2025-01-01 00:00:00', 'admin'),
     (9998, 'DB03', NULL, '2025-01-01 00:00:00', 'admin'),
@@ -201,7 +278,12 @@ INSERT into cancel(eno, excode, sno, cdate, cuser) VALUES
 
 -- Foreign Key Violation (Non-existent excode and Student already cancelled sno)
 -- Output:
--- 
+    /*
+    ERROR:  insert or update on table "cancel" violates foreign key constraint "fk_cancel_exam"
+    Key (excode)=(CODE) is not present in table "exam". 
+    ERROR:  insert or update on table "cancel" violates foreign key constraint "fk_cancel_student"
+    Key (sno)=(6435) is not present in table "student". 
+    */
     (9996, 'CODE', 9, '2025-01-01 00:00:00', 'admin'),
     (9995, 'DB02', 6435, '2025-01-01 00:00:00', 'admin');
 ---------------------------------------------
