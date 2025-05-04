4ccccdbf905255a6cde1584fb5b763cb95db517b
Electricmantaray <hlsj1066@gmail.com>
2025-05-04 04:35:54 +0100

    Indexes

--- Upload Files/100476042_DDL.sql
+++ Upload Files/100476042_DDL.sql
@@ -45,6 +45,25 @@ CREATE TABLE IF NOT EXISTS cancel (
 ----------------------------------------------
 
 
+------------------- INDEXES -------------------
+
+-- Primary keys indexed as they're often used im WHEN statements
+-- excode
+CREATE INDEX index_exam_code ON exam(excode);
+
+-- sno
+CREATE INDEX index_student_number ON student(sno));
+
+-- eno
+CREATE INDEX index_entry_number ON entry(eno);
+
+-- exdate
+-- Frequently filtered for, index used to speed up process
+CREATE INDEX index_exam_date ON exam(exdate);
+
+-----------------------------------------------
+
+
 ------------ FUNCTIONS / TRIGGER ------------
 
 -- Functions --
@@ -195,4 +214,53 @@ INNER JOIN entry ON exam.excode = entry.excode
 INNER JOIN student ON entry.sno = student.sno
 ORDER BY excode, sname
 
+-----------------------------------------------
+
+
+----------- TRANSACTIONS OF INTEREST -----------
+/*
+-- A
+INSERT INTO student VALUES
+	(11, 'Transaction Student', 'tstudent@example.edu');
+
+-- B
+INSERT INTO exam VALUES
+	('TB01', 'Transaction B', 'Online exam', '2025-11-29', '12:15');
+
+-- C
+BEGIN TRANSACTION;
+
+DELETE FROM student
+WHERE sname = 'Emily Parker';
+
+COMMIT;
+
+-- D
+BEGIN TRANSACTION;
+
+DELETE FROM exam
+WHERE excode = 'DB03';
+
+COMMIT;
+
+-- E
+INSERT INTO entry VALUES
+	(11, 'AI02', 10, NULL);
+
+-- F
+UPDATE entry
+SET egrade = 75.00
+WHERE eno = 11;
+
+-- G
+SELECT sname, excode, extitle, exlocation, exdate, extime FROM student_timetable
+WHERE sno = 10;
+
+-- H
+SELECT * FROM exam_results;
+
+-- I
+SELECT * FROM exam_results
+WHERE excode = 'DB01';
+*/
 -----------------------------------------------
